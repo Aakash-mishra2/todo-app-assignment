e171643478846f90a84ee0f126bb85bd43a0e163
Aakash Mishra <aakashmishra.281@gmail.com>
2023-10-03 17:59:42 +0530

    reducers logic 4 parts

--- src/boards/singleBoard.js
+++ src/boards/singleBoard.js
@@ -3,21 +3,27 @@ import Modal from "../shared/UIElements/Modal";
 import './styles/singleBoard.css';
 
 const SingleBoard = (props) => {
-
+    const handleChange = (event) => {
+         console.log(event.target.name);
+    }
     const [openBox, setOpenBox] = useState(false);
     const toggleBoard = () => setOpenBox(prev => !prev);
     return (
         <React.Fragment>
-
             <Modal
                 show={openBox}
                 closeBox={toggleBoard}
-                header={<p>{props.id}  {props.date}</p>}
+                header={<p>{props.id} : {props.date}</p>}
             >
                 <section className='alltasks'>
                     <div className='done'>
                         <h1>Incomplete Tasks</h1>
-                        {props.remTasks.map(x => <p>{x}</p>)}
+                        {props.remTasks.map(x => 
+                            <label htmlFor={x}>
+                            <b><em>{x}      </em></b>
+                            <input type='checkbox' name={x} onChange={handleChange} />
+                            </label>
+                        )}
                     </div>
                     <div className='notDone'>
                         <h1>Completed Tasks</h1>
--- src/features/boardSlice.js
+++ src/features/boardSlice.js
@@ -23,8 +23,25 @@ const boardSlice = createSlice({
     name: 'boards',
     initialState,
     reducers: {
-        addTodo: {},
-        deleteTodo: {},
+        addTodo: (state, action) => {
+            const newTask = action.payload.taskName;
+            const activeBoard = action.payload.boardName;
+            const selectedBoard = state.boards.findIndex((x) => x.id === activeBoard);
+            state.boards[selectedBoard].leftTodo.push(newTask);
+        },
+        deleteTodo: (state, action)=>{
+            const delTask = action.payload.taskName;
+            const activeBoard = action.payload.boardName;
+            const selectedBoard = state.boards.findIndex((x) => x.id === activeBoard);
+            state.boards[selectedBoard].leftTodo = state.boards[selectedBoard].leftTodo.filter((item) => item !== delTask);
+        },
+        completeTodo: (state, action) => {
+            const delTask = action.payload.taskName;
+            const activeBoard = action.payload.boardName;
+            const selectedBoard = state.boards.findIndex((x) => x.id === activeBoard);
+            state.boards[selectedBoard].leftTodo = state.boards[selectedBoard].leftTodo.filter((item) => item !== delTask);
+            state.boards[selectedBoard].doneTodo.push(delTask);
+        },
         createBoard: (state, action) => {
             const id = action.payload.name;
             const date = action.payload.date;
